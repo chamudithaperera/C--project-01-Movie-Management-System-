27b839178a6a4624c8a9673cc049ac86084b32f8
Chamuditha Kavishan Perera <139614066+chamudithaperera@users.noreply.github.com>
2024-01-22 22:11:27 +0530

    Update AddMovieDetails.cs

--- MovieManagementSystem/AddMovieDetails.cs
+++ MovieManagementSystem/AddMovieDetails.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
 
 namespace MovieManagementSystem
 {
